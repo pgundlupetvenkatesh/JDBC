ee6360b9f30cd57223eb3257f66ddc6a53565726
Pratik Gundlupet Venkatesh <pgundlupetvenkatesh@mail.csuchico.edu>
2016-09-10 16:36:09 -0700

    Making a connection to my local Orcl DB server and retrieving data.

---
+++ DatabaseExcercise/src/database/ConnectToOrcl.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author Pratik
+ *
+ */
+public class ConnectToOrcl {
+	@Test
+	public void jdbc() throws Exception {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection cins570 = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "<user_name>", "<password>");
+		Statement stmt = cins570.createStatement();
+		ResultSet rs = stmt.executeQuery("select * from employees");
+		ArrayList<String> al = new ArrayList<String>();
+		while(rs.next()) {
+			String name = rs.getString("FIRST_NAME");
+			al.add(name);
+		}
+		System.out.println("My first name is " + al.get(0));
+	}
+}
